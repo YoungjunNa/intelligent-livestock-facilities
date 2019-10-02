4e7db1ed188d84f04c39b5626d3b140f0916100d
Youngjun Na <ruminoreticulum@gmail.com>
2019-10-02 16:05:23 +0900

    Add files via upload

---
+++ Report 2.R
@@ -0,0 +1,52 @@
+# 연습문제 1. 다음 중 유효한 객체 이름은 무엇일까요?
+# 
+# 1. today
+# 2. 1st
+# 3. +3
+# 4. vars
+# 5. ^.^
+# 6. foo
+
+# Answer: 
+
+
+# 연습문제 2. 다음과 같이 copy라는 새 객체에 데이터를 할당하면 어떻게 될까요? 1) 코드를 실행하고 2) data와 copy의 결과를 비교해 보세요.
+# 
+# data <- rnorm(100, mean = 100, sd = 15)
+# copy <- data
+#
+# Answer 1: 
+
+
+
+# Answer 2: 
+
+
+
+
+  
+# 연습문제 3. 
+# 1) 아래 코드의 문제점은 무엇인가? 
+# 2) 어떻게 해야 코드가 동작할 수 있을까?
+#   
+# rnorm(n = 100, mu = 100, sd = 50)
+#
+# Answer: 
+
+
+
+
+
+# 연습문제 4. 다음 코드가 올바르게 실행되도록 조정하라
+#
+# livrary(ggplot2)
+# 
+# ggplot(dota = mpg) +
+#   geom_point(mapping = aes(x = displ, y = hwy))
+
+# Answer:
+
+
+
+
+
